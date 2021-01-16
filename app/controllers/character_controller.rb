1871e14998e34c2b16fbd26fefc10a77ab88c383
rmholsen <jaguar.kitty@gmail.com>
2021-01-15 20:30:31 -0500

    Updated characters with Revise/Delete functionality, bug hunt 5

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -9,10 +9,12 @@ class CharacterController < ApplicationController
         #erb :"/characters/index"
     #end 
     #THIS FORM OF CHARACTER INDEX NOW OBSOLETE
+    #I honestly don't know why I'm leaving this in here but I am. 
 
     get '/novels/:novel_slug/characters' do
     #Displays the list of characters in each novel
     @novel = Novel.find_by_slug(params[:novel_slug])
+    #Do I really care if other users can READ who's in what novel or ... decisions.
         if current_user.id == @novel.user_id 
             @characters = @novel.characters.all 
             erb :"/characters/index"
@@ -25,7 +27,11 @@ class CharacterController < ApplicationController
     #Loads form to create a new character for a specific novel
     @novel = Novel.find_by_slug(params[:novel_slug])
     @tropes = Trope.all
-    erb :"/characters/new"
+        if current_user.id == @novel.user_id 
+            erb :"/characters/new"
+        else 
+            erb :"/characters/index"
+        end 
     end 
 
     post '/novels/:novel_slug/characters' do 
@@ -40,9 +46,9 @@ class CharacterController < ApplicationController
             @character.save 
             #Creates a new character for a specific novel based on parameters
             #Renders the character index for the novel
-            redirect to "/novels/#{@novel.slug}/characters"
+            redirect to "/novels/#{@novel.slug}/characters/#{@character.slug}"
         else 
-            redirect to "/"
+            redirect to "/novels/#{@novel.slug}/characters"
             #flash message you bad person
         end 
     end 
@@ -51,16 +57,48 @@ class CharacterController < ApplicationController
     #Displays a specific character page
     @novel = Novel.find_by_slug(params[:novel_slug])
     @character = Character.find_by_slug(params[:slug])
-    @tropes = @character.tropes 
+    @tropes = @character.tropes
     erb :"/characters/show"
     end 
 
-    get '/characters/:slug/edit' do
-    #Shows the character name and the tropes associated
+    get '/novels/:novel_slug/characters/:slug/edit' do
+    @novel = Novel.find_by_slug(params[:novel_slug])
+    @character = Character.find_by_slug(params[:slug])
+    @tropes = Trope.all
+        if current_user.id == @novel.user_id 
+            #Shows the character name and the tropes associated
+            erb :"/characters/edit"
+        else
+            #No editing other people's characters you bad person 
+            redirect to "/novels/#{@novel.slug}/characters/#{@character.slug}"
+        end 
     end 
 
-    patch '/characters/:slug/edit' do
-    #Edits a characters tropes
-    #Does NOT edit the novel a character is assigned to, just offers to delete the character
+    patch '/novels/:novel_slug/characters/:slug' do
+    @novel = Novel.find_by_slug(params[:novel_slug])
+    @character = Character.find_by_slug(params[:slug])
+        if current_user.id == @novel.user_id
+            #Edits a characters tropes
+            #Does NOT edit the novel a character is assigned to, just offers to delete the character
+            @character.update(name: params[:name])
+            @character.tropes.clear
+            #This may not be the best way to do this but it's the simplest that I can think of right now?
+            @tropes = params[:trope_ids]
+            @tropes.each do |t|
+                @character.tropes << Trope.find(t)
+            end 
+            @character.save 
+            redirect to "/novels/#{@novel.slug}/characters/#{@character.slug}"
+        else 
+            #Flash message
+            redirect to "/novels/#{@novel.slug}/characters/#{@character.slug}"
+        end 
+    end 
+
+    delete '/novels/:novel_slug/characters/:slug' do
+        @novel = Novel.find_by_slug(params[:novel_slug])
+        @character = Character.find_by_slug(params[:slug])
+        @character.destroy
+        redirect to "/novels/#{@novel.slug}/characters"
     end 
 end 
